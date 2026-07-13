5d3f10b94a008fef70657f3b8e4dfe457f8a8f32
ignasi-sanchez <ignasi.sanchez@amaris.com>
2026-07-13 09:45:53 +0200

    initialize maven project

---
+++ backend/src/test/java/com/iignaasii47/e_commerce_api/ECommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iignaasii47.e_commerce_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
