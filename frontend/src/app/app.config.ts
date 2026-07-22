3d2049ad408e2d61748a0555d8e8ace60942b970
ignasi-sanchez <ignasi.sanchez@amaris.com>
2026-07-22 13:34:14 +0200

    updated frontend toast for too many requests error

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,11 +4,12 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { authInterceptor } from './services/auth.interceptor';
+import { rateLimitInterceptor } from './services/rate-limit.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withViewTransitions()),
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withInterceptors([authInterceptor, rateLimitInterceptor])),
   ],
 };
