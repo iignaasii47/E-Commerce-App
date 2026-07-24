c30260abe72aafa8ae15c1b168885ebdc1f48c30
ignasi-sanchez <ignasi.sanchez@amaris.com>
2026-07-24 13:35:12 +0200

    vitest fix on CI

--- frontend/vitest-base.config.ts
+++ frontend/vitest-base.config.ts
@@ -4,5 +4,11 @@ export default defineConfig({
   test: {
     globals: true,
     fileParallelism: false,
+    pool: 'forks',
+    poolOptions: {
+      forks: {
+        singleFork: true,
+      },
+    },
   },
 });
